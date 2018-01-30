7c150e72b240f9f2ef5d0092bbd1dc2bd7921cb5
Elijah <emadams92@gmail.com>
2018-01-30 05:59:04 -0500

    new components and routing examples

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -17,22 +17,28 @@ export class PostsService {
 
     return this.httpClient.get<Post[]>(this.postsUrl);
 
-  }
+  };
+
+  getPost(id : number) : Observable<Post> {
+    const url =`${this.postsUrl}/${id}`;
+    return this.httpClient.get<Post>(url);
+  };
+
 
   savePost(post : Post) : Observable<Post> {
     return this.httpClient.post<Post>(this.postsUrl, post, httpOptions);
-  }
+  };
 
   updatePost(post: Post) : Observable<Post> {
     const url =`${this.postsUrl}/${post.id}`;
     return this.httpClient.put<Post>(url, post, httpOptions);
 
-  }
+  };
 
   removePost(post: Post | number ) : Observable<Post> {
     const id = typeof post === 'number' ? post : post.id;
     const url =`${this.postsUrl}/${id}`;
 
     return this.httpClient.delete<Post>(url, httpOptions);
-  }
+  };
 }
